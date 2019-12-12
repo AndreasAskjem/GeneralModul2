d38af36f7f79f9795faf1da15b46f17081c53f9a
AndreasAskjem <andreasa@getacademy.com>
2019-12-12 16:11:07 +0100

    The fix to the tail-crash bug seems to finally be working.

--- snakeTwoPlayer/controller.js
+++ snakeTwoPlayer/controller.js
@@ -34,23 +34,10 @@ function controlSnake(e){
         if (e.keyCode == 83 && d1.y != -1) { // down
             model.snakes[1].nextDirection = { y: 1, x: 0 };
         }
-    } catch{}
+    } catch{return}
 }
 
-/*
-Order of events
-Go through each part for every snake, instead of every part for each snake
-Find next head positoins (check crash with wall?) -> return list
-Eat apples -> Remove tail or not
-Check collisions with wall
-Check collision with snakes
-If crashed -> remove
-Insert head, placeSnake()
 
-Check winners and such
-*/
-
-/////////////////
 
 function move(){
     let result = [];
@@ -61,8 +48,9 @@ function move(){
         result[i].crashed = checkCrashWithWall(result[i].head, i)
     })
     model.snakes.forEach((snake, i) => {
-        //if(result[i])
-        result[i].ateApple = ateApple(result[i].head, i);
+        if(!result[i].crashed){
+            result[i].ateApple = ateApple(result[i].head, i);
+        }
     })
     model.snakes.forEach((snake, i) => {
         if(!result[i].ateApple && !result[i].crashed){
@@ -70,7 +58,9 @@ function move(){
         }
     })
     model.snakes.forEach((snake, i) => {
-        result[i].crashed = checkCrashWithSnake(result[i].head) || result[i].crashed;
+        if(!result[i].crashed){
+            result[i].crashed = checkCrashWithSnake(result[i].head);
+        }
     })
 
     for(let i=0; i<gameState.players-1; i++){
@@ -81,6 +71,19 @@ function move(){
             }
         }
     }
+
+    model.snakes.forEach((snake, i) => {
+        if(!result[i].crashed){
+            snake.position.splice(0, 0, result[i].head);
+        }
+    })
+
+    model.snakes.forEach((snake, i) => {
+        if(!result[i].crashed){
+            placeSnake(snake, i);
+        }
+    })
+
 
 
     let listOfCrashes = result.map(s => s.crashed);
@@ -93,7 +96,7 @@ function move(){
     //console.log(result);
     let colors = ['Blue', 'Green'];
     for(let i=0; i<gameState.players; i++){
-        if(result[i].points >= gameState.winningScore){
+        if(model.snakes[i].size - gameState.startLength >= gameState.winningScore){
             winnerTxt = `${colors[i]} won!`;
             showBoard();
             stopMove();
@@ -113,17 +116,6 @@ function move(){
         return;
     }
 
-    model.snakes.forEach((snake, i) => {
-        if(!result[i].crashed){
-            snake.position.splice(0, 0, result[i].head);
-        }
-    })
-
-    model.snakes.forEach((snake, i) => {
-        if(!result[i].crashed){
-            placeSnake(snake, i);
-        }
-    })
 
     result.forEach(s => {if(s.ateApple){placeApple()}})
     showBoard();
@@ -151,106 +143,6 @@ function removeTail(snake, i){
     let tail = snake.position.splice(snake.size -1, 1)[0];
     model.board.rows[tail.y].cells[tail.x].hasBody = false;
     model.board.rows[tail.y].cells[tail.x].anyBody = false;
-}
-
-///////////////////
-/*
-let listOfTails = undefined;
-function move(){
-    let result = []
-    
-    for(let snake=0; snake<gameState.players; snake++){
-        if(snake===0){
-            listOfTails = undefined;
-        }
-        result.push(moveSnake(model.snakes[snake], snake));
-    }
-    listOfTails = result.map(s => s.lastTail)//////////////////
-    //console.log(listOfTails)
-    result.forEach(s => {if(s.ateApple){placeApple()}})
-
-    // Marks both snakes as crashed if their heads move into the same area at the same time.
-    for(let i=0; i<gameState.players-1; i++){
-        for(let j=i+1; j<gameState.players; j++){
-            if(result[i].head.x===result[j].head.x && result[i].head.y===result[j].head.y){
-                result[i].crashed = true;
-                result[j].crashed = true;
-            }
-        }
-    }
-    
-    let listOfCrashes = result.map(p => p.crashed);
-    let livingSnakes = 0;
-    for(let i=0; i<gameState.players; i++){
-        if(!result[i].crashed){
-            livingSnakes++;
-        }
-    }
-    console.log(result);
-    let colors = ['Blue', 'Green'];
-    for(let i=0; i<gameState.players; i++){
-        if(result[i].points >= gameState.winningScore){
-            winnerTxt = `${colors[i]} won!`;
-            showBoard();
-            stopMove();
-            return;
-        }
-    }
-
-    let winner;
-    if(livingSnakes===1 && gameState.players>1){
-        winner = listOfCrashes.indexOf(false);
-        winnerTxt = `${colors[winner]} won!`;
-        stopMove();
-        return;
-    }
-    else if(livingSnakes===0){
-        winnerTxt = `It's a tie!`;
-        stopMove();
-        return;
-    }
-
-    showBoard();
-}
-*/
-
-function moveSnake(snake, index){
-    let head = snake.position[0];
-
-    let result = {ateApple: false, crashed: false};
-    result.lastTail = snake.position[snake.position.length-1];
-    snake.direction = checkDirection(snake.direction, snake.nextDirection);
-    let d = snake.direction;
-
-    let newHead = {y: head.y + d.y, x: head.x + d.x}
-    result.head = newHead;
-    result.points = snake.position.length - gameState.startLength;
-    if(checkCrashWithWall(newHead)){
-        result.crashed = true;
-        return(result);
-    }
-
-    snake.position.splice(0, 0, newHead);
-    if(model.board.rows[newHead.y].cells[newHead.x].hasApple){
-        model.board.rows[newHead.y].cells[newHead.x].hasApple = false;
-        snake.size++;
-        result.points++;
-        result.ateApple = true;
-    }
-    else{
-        let tail = snake.position.splice(snake.size, 1)[0];
-        model.board.rows[tail.y].cells[tail.x].hasBody = false;
-        model.board.rows[tail.y].cells[tail.x].anyBody = false;
-        result.ateApple = false;
-    }
-
-    if(checkCrashWithSnake(newHead)){
-        result.crashed = true;
-        return(result);
-    } //////////////////////////// Fix tail collision
-
-    placeSnake(snake, index);
-    return(result);
 }
 
 function checkCrashWithWall(head){
